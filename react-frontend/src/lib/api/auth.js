72e2094d9f93ede66f6d9cbef3e53d61f0a018d5
wonmi1203 <wonmijang1203@gmail.com>
2023-03-03 19:52:56 +0900

    sdfsdfasdfas

--- react-frontend/src/lib/api/auth.js
+++ react-frontend/src/lib/api/auth.js
@@ -6,7 +6,13 @@ export const login = ({ username, password }) =>
 
 // 회원가입
 export const register = ({ username, password }) =>
-  client.post('/api/auth/register', { username, password });
+  client.post('/api/auth/register', {
+    username, password,
+    headers: {
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Origin": "*"
+    }
+  });
 
 // 로그인 상태 확인
 export const check = () => client.get('/api/auth/check');
