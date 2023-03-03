3ec2496670969ea2d13bbeb0b35839fe3d09099c
wonmi1203 <wonmijang1203@gmail.com>
2023-03-03 19:58:56 +0900

    ateestawetse

--- react-frontend/src/setupProxy.js
+++ react-frontend/src/setupProxy.js
@@ -1,14 +1,14 @@
 // module import -> npm install http-proxy-middleware
 // src/setupProxy.js
-//const { createProxyMiddleware } = require("http-proxy-middleware");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 // src/setupProxy.js
-//module.exports = function (app) {
-//  app.use(
- //   createProxyMiddleware("/api", {
-  //    target: "http://18.212.94.168:80", // 비즈니스 서버 URL 설정
-   //   changeOrigin: true,
-   //   withCredentials: true
-  //  })
- // );
-//};
+module.exports = function (app) {
+  app.use(
+    createProxyMiddleware("/api", {
+      target: "http://18.212.94.168:4000", // 비즈니스 서버 URL 설정
+      changeOrigin: true,
+      withCredentials: true
+    })
+  );
+};
