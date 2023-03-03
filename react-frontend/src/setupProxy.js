4249a3eca9abdf78c93466759a32d21c4b74ef38
wonmi1203 <wonmijang1203@gmail.com>
2023-03-03 17:17:47 +0900

    qusrud

--- react-frontend/src/setupProxy.js
+++ react-frontend/src/setupProxy.js
@@ -6,7 +6,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
     createProxyMiddleware("/api", {
-      target: "http://localhost:4000", // 비즈니스 서버 URL 설정
+      target: "http://18.212.94.168:4000", // 비즈니스 서버 URL 설정
       changeOrigin: true,
       withCredentials: true
     })
