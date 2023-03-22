0e4b0404c41a39dffa212a6465cf22f432633bb3
최원석 <cws4987@siwonschool.com>
2023-03-22 09:21:13 +0900

    대댓글 기능 추가

--- react-frontend/src/components/comment/CommentForm.js
+++ react-frontend/src/components/comment/CommentForm.js
@@ -4,22 +4,45 @@ class CommentForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      comment: ''
+      comment: '',
+      showInput: false
     };
   }
 
   render() {
-    return (
-      <form onSubmit={this.handleSubmit.bind(this)}>
-        <input
-          type="text"
-          placeholder="댓글을 입력하세요..."
-          value={this.state.comment}
-          onChange={this.handleInputChange.bind(this)}
-        />
-        <button type="submit">등록</button>
-      </form>
-    );
+    if (this.props.parentId == null || this.state.showInput) {
+      const placeholderText = this.props.parentId != null ? '답글을 입력하세요' : '댓글을 입력하세요';
+      const cancelButton = this.props.parentId != null ? true : false
+      let button;
+      if (cancelButton) {
+        button = <button onClick={() => this.setState({ showInput: false })}>
+        &nbsp;&nbsp;&nbsp;취소
+      </button>
+      } else {
+        button = null;
+      }
+      return (
+        <form onSubmit={this.handleSubmit.bind(this)}>
+          <input
+            type="text"
+            placeholder={placeholderText}
+            value={this.state.comment}
+            onChange={this.handleInputChange.bind(this)}
+          />
+          <button type="submit">등록</button>
+          {button}
+        </form>
+      );
+    } else {
+      if (this.props.reply) {
+        return null
+      }
+      return (
+        <button onClick={() => this.setState({ showInput: true })}>
+          &nbsp;&nbsp;&nbsp;답글 작성
+        </button>
+      );
+    }
   }
 
   handleInputChange(event) {
@@ -27,9 +50,10 @@ class CommentForm extends Component {
   }
 
   handleSubmit(event) {
+    console.log("handleSubmit " + this.state.comment)
     event.preventDefault();
-    this.props.addComment(this.state.comment);
-    this.setState({ comment: '' });
+    this.props.addComment(this.state.comment, this.props.parentId);
+    this.setState({ comment: '', showInput: false });
   }
 }
 
--- react-frontend/src/components/comment/CommentList.js
+++ react-frontend/src/components/comment/CommentList.js
@@ -1,12 +1,51 @@
 import React from 'react';
+import CommentForm from './CommentForm';
 
 const CommentList = (props) => {
+  const comments = props.comments;
+  const addComment = props.addComment;
+
+  const getChildComments = (parentId) => {
+    return comments.filter((comment) => comment.parentId === parentId);
+  };
+
+  const renderComments = (parentId) => {
+    const childComments = getChildComments(parentId);
+    if (childComments.length === 0) {
+      return null;
+    }
+    return (
+      <ul>
+        {childComments.map((comment) => {
+          return (
+            <li key={comment.id}>
+              <div>
+                {"\u00A0ㄴ"}
+                {comment.text}
+              </div>
+              <CommentForm parentId={comment.id} addComment={addComment} reply={true} />
+            </li>
+          );
+        })}
+      </ul>
+    );
+  };
+
   return (
-    <ul>
-      {props.comments.map((comment, index) => {
-        return <li key={index}>{comment}</li>;
+    <div>
+      {comments.map((comment) => {
+        if (comment.parentId === null) {
+          return (
+            <div key={comment.id}>
+              {comment.text}
+              <CommentForm parentId={comment.id} addComment={addComment} reply={false} />
+              {renderComments(comment.id)}
+            </div>
+          );
+        }
+        return null;
       })}
-    </ul>
+    </div>
   );
 };
 
