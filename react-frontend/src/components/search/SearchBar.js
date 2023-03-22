c3ed988b5360953742a5688366ea429e9cdec23d
yujin000915 <yujin3717@naver.com>
2023-03-22 11:16:20 +0900

    검색 Bar UI 변경

--- react-frontend/src/components/search/SearchBar.js
+++ react-frontend/src/components/search/SearchBar.js
@@ -1,33 +1,63 @@
 import React, { useState, useCallback } from 'react';
+import styled from 'styled-components';
+import Button from '../common/Button';
 
+const SearchBlock = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: flex-end;
+	width: 1024px;
+	margin: 0 auto;
+	padding: 2rem;
+
+	#keyword {
+		width: 500px;
+		height: 3rem;
+		padding: 0 0.5rem;
+		border: none;
+		border-bottom: 1px solid #000;
+		outline: none;
+	}
+
+	.search {
+		margin-left: 0.5rem;
+		font-weight: 400;
+	}
+`;
 
 const SearchBar = ({ history }) => {
-    const [keyword, setSearch] = useState('');
+	const [keyword, setSearch] = useState('');
 
-    const onChange = useCallback(e => {
-        setSearch(e.target.value);
-    }, []);
+	const onChange = useCallback(e => {
+		setSearch(e.target.value);
+	}, []);
 
-    const onSubmit = useCallback(
-        e => {
-            const searchKeyword = document.querySelector('#keyword').value;
-            console.log(searchKeyword);
-            //  history.push(`/search/${keyword}`);
-            setSearch("");
-        }, []);
+	const onSubmit = useCallback(
+		e => {
+			const searchKeyword = document.querySelector('#keyword').value;
+			console.log(searchKeyword);
+			//  history.push(`/search/${keyword}`);
+			setSearch("");
+		}, []);
 
-    return (
-        <div className="header">
-            <input id="keyword"
-                placeholder="검색어를 입력하세요"
-                value={keyword}
-                onChange={onChange}
-            />
-            <button type="button" className="search" onClick={onSubmit}>
-                <span>검색</span>
-            </button>
-        </div>
-    )
+	return (
+		<SearchBlock>
+			<input
+				id="keyword"
+				placeholder="검색어를 입력하세요"
+				value={keyword}
+				onChange={onChange}
+			/>
+
+			<Button
+				type="button"
+				className="search"
+				onClick={onSubmit}
+			>
+				검색
+			</Button>
+		</SearchBlock>
+	)
 };
 
 export default SearchBar;
