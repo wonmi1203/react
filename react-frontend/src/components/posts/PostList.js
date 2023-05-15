b9c986fad0ae0315a7289c4f413c3108615f68d7
yujin000915 <yujin3717@naver.com>
2023-05-15 10:54:46 +0900

    상세 주석추가

--- react-frontend/src/components/posts/PostList.js
+++ react-frontend/src/components/posts/PostList.js
@@ -1,13 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import styled from 'styled-components';
-import Responsive from '../common/Responsive';
-import Button from '../common/Button';
-import palette from '../../lib/styles/palette';
-import SubInfo from '../common/SubInfo';
-import Tags from '../common/Tags';
-import { Link } from 'react-router-dom';
-import InfiniteScroll from 'react-infinite-scroll-component';
-import queryString from 'query-string';
+import React, { useState, useEffect } from 'react'; // useState, useEffect 사용
+import styled from 'styled-components'; // css style 사용
+import Responsive from '../common/Responsive'; // 반응형 사용
+import Button from '../common/Button'; // 공통 button Component
+import palette from '../../lib/styles/palette'; // 공통 색상 Component
+import SubInfo from '../common/SubInfo'; // 공통 사용자 정보 레이아웃 Component
+import Tags from '../common/Tags';  // 해시태그 레이아웃 Component
+import { Link } from 'react-router-dom'; // link tag 사용
+import InfiniteScroll from 'react-infinite-scroll-component'; // 인피니티 스크롤 사용
+import queryString from 'query-string'; // queryString 받아오기
 
 const PostListBlock = styled(Responsive)`
 	margin-top: 3rem;
@@ -52,10 +52,10 @@ const PostItemBlock = styled.div`
 	}
 `;
 
-let search = queryString.parse(window.location.search);
+let search = queryString.parse(window.location.search); // 현 페이지 url에서 쿼리 문자열 추출
 
-if (search) {
-	search = search.search;
+if (search) { // search 값이 true 일때
+	search = search.search; // search 변수에 search 값 재할당
 }
 
 const PostItem = ({ post }) => {
@@ -64,28 +64,32 @@ const PostItem = ({ post }) => {
 	return (
 		<PostItemBlock>
 			<h2>
+				{/* 글 제목 */}
 				<Link to={`/@${user.username}/${_id}`}>{title}</Link>
 			</h2>
+			{/* 사용자 정보 컴포넌트 */}
 			<SubInfo username={user.username} publishedDate={new Date(publishedDate)} />
+			{/* 해시태그 컴포넌트 */}
 			<Tags tags={tags} />
+			{/* 글 내용 */}
 			<p>{body}</p>
 		</PostItemBlock>
 	);
 };
 
 const PostList = ({ posts, loading, error, showWriteButton }) => {
-	const [items, setItems] = useState(posts?.slice(0, 3) || []); // 먼저 보여줄 데이터 갯수, 데이터가 존재하면 (0~3) or []
-	const [hasMore, setHasMore] = useState(posts?.length > 3); // 더 가져올 데이터 갯수
+	const [items, setItems] = useState(posts?.slice(0, 3) || []); // 먼저 보여줄 데이터 갯수, 데이터가 3개이상일 경우 보내고 그렇지 않다면 빈배열 넘겨줌.
+	const [hasMore, setHasMore] = useState(posts?.length > 3); // 데이터가 3보다 크면 true.
 
-	useEffect(() => {
+	useEffect(() => { // 데이터가 변경되면 아래가 실행됨
 		if (posts) {
-			setItems(posts.slice(0, 3));
-			setHasMore(posts.length > 3);
+			setItems(posts.slice(0, 3)); // 3번째 까지 자름
+			setHasMore(posts.length > 3); // 3보다 큰지 확인
 		}
 	}, [posts]);
 
 	const fetchMoreData = () => {
-		if (!posts) return;
+		if (!posts) return; // 데이터가 존재하지 않으면 함수 중단
 
 		setTimeout(() => {
 			const startIndex = items.length;
@@ -99,16 +103,16 @@ const PostList = ({ posts, loading, error, showWriteButton }) => {
 				newItems = posts.slice(startIndex, startIndex + 3);
 				setHasMore(true); // 데이터 더 가져오기
 			}
-			setItems((prevItems) => [...prevItems, ...newItems]);
-		}, 500);
+			setItems((prevItems) => [...prevItems, ...newItems]); // 이전것과 새로운것을 합쳐 배열 생성
+		}, 500); // 0.5초 뒤 실행
 	};
 
 
-	if (error) {
+	if (error) { // 에러 발생시 실행
 		return <PostListBlock>에러가 발생했습니다.</PostListBlock>;
 	}
 
-	if (!loading && !Array.isArray(posts)) {
+	if (!loading && !Array.isArray(posts)) { // 로딩중이 아니고 post 데이터가 없으면 실행
 		return <PostListBlock>게시글이 없습니다.</PostListBlock>;
 	}
 
@@ -120,7 +124,7 @@ const PostList = ({ posts, loading, error, showWriteButton }) => {
 				)}
 			</WritePostButtonWrapper>
 
-			{!loading && Array.isArray(posts) && (
+			{!loading && Array.isArray(posts) && ( // 로딩중이 아니고 posts 데이터가 있으면 실행
 				<InfiniteScroll
 					style = {{ overflow: 'hidden' }}
 					dataLength = {items.length} // 데이터 길이
@@ -134,6 +138,7 @@ const PostList = ({ posts, loading, error, showWriteButton }) => {
 				</InfiniteScroll>
 			)}
 
+			{/* items 가 0개면 게시글 없습니다 노출 */}
 			{items.length === 0 && <PostListBlock>게시글이 없습니다.</PostListBlock>}
 		</PostListBlock>
 	);
--- react-frontend/src/setupProxy.js
+++ react-frontend/src/setupProxy.js
@@ -6,7 +6,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
 	app.use(
 		createProxyMiddleware("/api", {
-			target: "http://13.125.54.163:4000", // 비즈니스 서버 URL 설정
+			target: "http://localhost:4000", // 비즈니스 서버 URL 설정
 			changeOrigin: true,
 			withCredentials: true
 		})
